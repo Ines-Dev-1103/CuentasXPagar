b46bb917ed663244a868dbe0f75b48a745a3dce0
Clara Ines <clarainesdiazangeles11@gmail.com>
2022-03-24 00:25:34 -0400

    Iniciando el proyecto

---
+++ Controllers/LoginDatabaseController.cs
@@ -0,0 +1,28 @@
+﻿using CuentasxPagar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq; 
+using System.Web;
+using System.Web.Mvc;
+
+namespace CuentasxPagar.Controllers
+{
+    public class LoginDatabaseController : Controller
+    {
+        // GET: LoginDatabase
+        public ActionResult Index()
+        {
+            //DataBase
+            Database database = new Database();
+            var listausuarios = database.Login.ToList();
+            
+
+            return new JsonResult
+            {
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                Data = listausuarios
+
+            };
+        }
+    }
+}
